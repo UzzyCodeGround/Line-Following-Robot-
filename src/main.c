dffcdf31dcd5dc5a273d253c62e5ec614f6124a8
UzzyCodeGround <162308749+UzzyCodeGround@users.noreply.github.com>
2024-03-17 15:42:26 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,69 +1,48 @@
-/*
- * This file is part of the µOS++ distribution.
- *   (https://github.com/micro-os-plus)
- * Copyright (c) 2014 Liviu Ionescu.
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom
- * the Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
+// Written by:
+// Date created:
+// Programmed in: STM32CubeIDE
+// Target: STM32F051
+// Description:
 
-// ----------------------------------------------------------------------------
+// DEFINES AND INCLUDES
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "diag/trace.h"
 
-// ----------------------------------------------------------------------------
-//
-// Standalone STM32F0 empty sample (trace via DEBUG).
-//
-// Trace support is enabled by adding the TRACE macro definition.
-// By default the trace messages are forwarded to the DEBUG output,
-// but can be rerouted to any device or completely suppressed, by
-// changing the definitions required in system/src/diag/trace-impl.c
-// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
-//
+#define STM32F051
+#include "stm32f0xx.h"
+#include "lcd_stm32f0.h"
+#define HIGH 1
+#define LOW 0
 
-// ----- main() ---------------------------------------------------------------
+// GLOBAL VARIABLES
+enum motion{back=-1, stop, forward, right, left};
+struct robot_status{
+	enum motion movement;
+	_Bool ir_left;
+	_Bool ir_right;
+} robot_state;
 
-// Sample pragmas to cope with warnings. Please note the related line at
-// the end of this function, used to pop the compiler diagnostics status.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
-#pragma GCC diagnostic ignored "-Wreturn-type"
 
-int
-main(int argc, char* argv[])
+// GLOBAL CONSTANTS
+
+
+// FUNCTION DECLARATIONS
+void main(void);
+
+
+// MAIN FUNCTION
+void main(void)
 {
-  // At this stage the system clock should have already been configured
-  // at high speed.
+	// Power on phase
+	// Robot_state initialised
 
-  // Infinite loop
-  while (1)
-    {
-       // Add your code here.
-    }
+
+	robot_state.movement = stop;
+	robot_state.ir_left = LOW;
+	robot_state.ir_right = LOW;
+
+	while(1){
+
+	}
 }
 
-#pragma GCC diagnostic pop
-
-// ----------------------------------------------------------------------------
+// FUNCTION DEFINITIONS
